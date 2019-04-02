c6e2429017313978cf3a76985ae42553a64af8fb
Samantha brake <sammy.brake@gmail.com>
2019-04-02 18:11:25 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    binding.pry
+    redirect_to student_path(@student)
   end
 
 end
